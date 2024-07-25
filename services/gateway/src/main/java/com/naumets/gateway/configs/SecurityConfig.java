e21de8419cb00a91a0e9b2ef3f4ee44342bdf7c6
Artem <you@example.com>
2024-07-25 21:38:55 +0300

    add cors configurations to gateway service

--- services/gateway/src/main/java/com/naumets/gateway/configs/SecurityConfig.java
+++ services/gateway/src/main/java/com/naumets/gateway/configs/SecurityConfig.java
@@ -6,21 +6,33 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
+                .cors(corsSpec -> corsSpec
+                        .configurationSource(request -> {
+                            org.springframework.web.cors.CorsConfiguration corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                            corsConfig.addAllowedOrigin("http://localhost:3000");
+                            corsConfig.addAllowedMethod("*");
+                            corsConfig.addAllowedHeader("*");
+                            corsConfig.setAllowCredentials(true);
+                            return corsConfig;
+                        }))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-                        .pathMatchers("(/eureka/**)")
+                        .pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated()
                 )
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
-        return serverHttpSecurity.build();
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .build();
     }
 }
